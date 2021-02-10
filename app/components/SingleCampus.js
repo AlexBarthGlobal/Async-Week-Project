82f7df1628e6926086eb8a8eec29632c4e252e1a
Alex Barth <alexNYCbarth@gmail.com>
2021-02-10 01:16:27 -0500

    Disabling image until CSS

--- app/components/SingleCampus.js
+++ app/components/SingleCampus.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+
+//props.imagee before fixing CSS
 const SingleCampus = (props) => {
    
     return(
         <div>
         <div id="image">
-        <Link to={`/campuses/${props.listId}`}><img src={props.image} alt="new"></img></Link>
+        <Link to={`/campuses/${props.listId}`}><img src={props.imagee} alt="image"></img></Link>
         </div>
         <Link to={`/campuses/${props.listId}`}>{props.name}</Link>
         </div>    
